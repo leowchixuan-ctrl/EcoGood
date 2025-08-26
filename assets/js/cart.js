d4b4edede55d0d837701290bdc6819a8fe7716ca
otakuhub <leowchixuan@1utar.my>
2025-08-26 18:40:50 +0800

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -11,20 +11,20 @@ function renderCart(){
     items.forEach(it => {
       const line = document.createElement('div');
       line.className = 'd-flex align-items-center justify-content-between p-3 border rounded-4';
-      const subtotal = it.price * it.qty;
-      total += subtotal;
-      line.innerHTML = `
-        <div class="d-flex align-items-center gap-3">
-          <img src="${it.image}" alt="" width="64" height="64" style="object-fit:contain" class="rounded">
-          <div>
-            <div class="fw-semibold">${it.title}</div>
-            <div class="small text-muted">Qty: <input type="number" min="1" value="${it.qty}" data-id="${it.id}" class="form-control form-control-sm d-inline-block" style="width:90px"></div>
-          </div>
-        </div>
-        <div class="text-end">
-          <div class="price">${formatMYR(subtotal)}</div>
-          <button class="btn btn-sm btn-outline-dark mt-2" data-remove="${it.id}">Remove</button>
-        </div>`;
+     const subtotal = (it.price * 4.7) * it.qty;
+total += subtotal;
+line.innerHTML = `
+  <div class="d-flex align-items-center gap-3">
+    <img src="${it.image}" alt="" width="64" height="64" style="object-fit:contain" class="rounded">
+    <div>
+      <div class="fw-semibold">${it.title}</div>
+      <div class="small text-muted">Qty: <input type="number" min="1" value="${it.qty}" data-id="${it.id}" class="form-control form-control-sm d-inline-block" style="width:90px"></div>
+    </div>
+  </div>
+  <div class="text-end">
+    <div class="price">${formatMYR(subtotal)}</div>
+    <button class="btn btn-sm btn-outline-dark mt-2" data-remove="${it.id}">Remove</button>
+  </div>`;
       container.appendChild(line);
     });
   }
